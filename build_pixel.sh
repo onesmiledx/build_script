8ded4153ea60c53f4b91d7ac1a1ace3d0a5f4d0e
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-08-04 12:22:28 +0500

    Create build_pixel.sh

---
+++ build_pixel.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#removals
+rm -rf .repo/local_manifests
+
+#sync
+repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs --depth=1
+git clone https://github.com/onesmiledx/local_manifest -b pixelos .repo/local_manifests
+if [ -f /opt/crave/resync.sh ]; then
+  /opt/crave/resync.sh
+else
+  repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+fi
+
+#custom repos
+rm -rf hardware/samsung
+git clone https://github.com/onesmiledx/android_hardware_samsung hardware/samsung
+
+#build
+. build/envsetup.sh
+lunch aosp_a71-ap2a-user
+mka installclean
+mka bacon
