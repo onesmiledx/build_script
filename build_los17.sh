f8bff478353657fbe244927760754ac4ee05106f
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-08-05 18:34:56 +0500

    Create build_los17.sh

---
+++ build_los17.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#removals
+rm -rf .repo/local_manifests
+
+#sync
+repo init -u https://github.com/LineageOS-Revived/android.git -b lineage-17.1 --git-lfs --depth=1
+git clone https://github.com/onesmiledx/local_manifest -b a10s .repo/local_manifests
+if [ -f /opt/crave/resync.sh ]; then
+  /opt/crave/resync.sh
+else
+  repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+fi
+
+#build
+. build/envsetup.sh
+brunch a10s
