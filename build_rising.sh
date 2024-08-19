31d75c56e4dab8efd7dcd4ec9d784db06636b6d1
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-08-20 03:48:06 +0500

    Create build_rising.sh

---
+++ build_rising.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#removals
+rm -rf .repo/local_manifests
+
+#sync
+repo init -u https://github.com/RisingTechOSS/android.git -b fourteen --git-lfs --depth=1
+git clone https://github.com/onesmiledx/local_manifest -b los21 .repo/local_manifests
+if [ -f /opt/crave/resync.sh ]; then
+  /opt/crave/resync.sh
+else
+  repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+fi
+
+#build
+. build/envsetup.sh
+export RISING_MAINTAINER=Starlix
+export RISING_CHIPSET=Snapdragon 730 4G
+riseup a71 userdebug
+rise b
