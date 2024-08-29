31c69ba04fe7839bb12ef7cbc0834fb2f0c989fa
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-08-29 17:45:07 +0500

    Update build_los21.sh

--- build_los21.sh
+++ build_los21.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#init
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+mkdir ~/bin
+PATH=~/bin:$PATH
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+
 #removals
 rm -rf .repo/local_manifests
 
