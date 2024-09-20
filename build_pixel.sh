11d92fcb8724bc09d23ac06f161b4eb9e6f704f1
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-09-20 18:28:51 +0500

    Update build_pixel.sh

--- build_pixel.sh
+++ build_pixel.sh
@@ -4,7 +4,7 @@
 rm -rf .repo/local_manifests
 
 #sync
-repo init -u https://github.com/PixelOS-AOSP/manifest.git -b fourteen --git-lfs --depth=1
+repo init -u repo init -u https://github.com/SuperiorOS/manifest.git -b fourteen --git-lfs --depth=1
 git clone https://github.com/onesmiledx/local_manifest -b los21 .repo/local_manifests
 if [ -f /opt/crave/resync.sh ]; then
   /opt/crave/resync.sh
@@ -14,6 +14,6 @@ fi
 
 #build
 . build/envsetup.sh
-lunch lineage_a71-ap2a-userdebug
+lunch aosp_a71-ap2a-userdebug
 mka installclean
 mka bacon
