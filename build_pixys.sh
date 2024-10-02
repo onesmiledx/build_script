638e1e18017bafab55b0e84009bffc16b9802b78
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-10-03 01:01:27 +0500

    Update build_pixys.sh

--- build_pixys.sh
+++ build_pixys.sh
@@ -2,6 +2,12 @@
 
 #removals
 rm -rf .repo/local_manifests
+rm -rf device/samsung/a71
+rm -rf device/samsung/a71-common
+rm -rf hardware/qcom-caf/common
+rm -rf kernel/samsung/a71
+rm -rf vendor/samsung/a71
+rm -rf vendor/samsung/a71-common
 
 #sync
 repo init -u https://github.com/PixysOS/manifest -b fourteen-v3 --depth=1
