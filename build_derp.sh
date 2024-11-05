f8adf597fbfb0324920a9a4c477565cc56f1f74f
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-11-05 21:54:32 +0500

    Update build_derp.sh

--- build_derp.sh
+++ build_derp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#removals
+# Removals
 rm -rf .repo/local_manifests
 
-#sync
+# ROM Sync
 repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 15 --git-lfs --depth=1
 git clone https://github.com/a71-aosp/local_manifest -b fifteen .repo/local_manifests
 if [ -f /opt/crave/resync.sh ]; then
@@ -12,7 +12,12 @@ else
   repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 fi
 
-#build
+# Setup env
 . build/envsetup.sh
 lunch derp_a71-userdebug
+
+# Clean
+make installclean
+
+# Run
 mka derp
