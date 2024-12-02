ed68025803122d382a7bc4fc2eebd833682dfd04
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-12-02 21:48:47 +0500

    Update build_pixel.sh

--- build_pixel.sh
+++ build_pixel.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-#removals
-rm -rf .repo/local_manifests
-
-#sync
-repo init -u https://github.com/onesmiledx/android_manifests.git -b 15 --git-lfs --depth=1
-git clone https://github.com/PixelOS-A71/local_manifests -b fifteen .repo/local_manifests
-/opt/crave/resync.sh
-
 #build
 rm -rf hardware/qcom-caf/sm6225
+rm -rf hardware/qcom-caf/sm8550
 . build/envsetup.sh
 lunch feather_a71-ap3a-user
 mka installclean
