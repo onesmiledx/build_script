9a75849b02144f96de06713cc7b489466b7b54f9
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-12-02 06:26:39 +0500

    Update build_pixel.sh

--- build_pixel.sh
+++ build_pixel.sh
@@ -4,7 +4,7 @@
 rm -rf .repo/local_manifests
 
 #sync
-repo init -u https://github.com/FeatherROM/android_manifests.git -b 15 --git-lfs --depth=1
+repo init -u https://github.com/onesmiledx/android_manifests.git -b 15 --git-lfs --depth=1
 git clone https://github.com/PixelOS-A71/local_manifests -b fifteen .repo/local_manifests
 if [ -f /opt/crave/resync.sh ]; then
   /opt/crave/resync.sh
