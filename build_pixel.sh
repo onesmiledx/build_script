8ed8fdcbdf961725a82ba79caac5d1d9013fd71e
Starlix <57674494+onesmiledx@users.noreply.github.com>
2024-12-04 07:05:44 +0500

    Update build_pixel.sh

--- build_pixel.sh
+++ build_pixel.sh
@@ -3,6 +3,8 @@
 #build
 rm -rf hardware/qcom-caf/sm6225
 rm -rf hardware/qcom-caf/sm8550
+rm -rf device/samsung/a71
+git clone https://github.com/PixelOS-A71/device_samsung_a71 device/samsung/a71
 . build/envsetup.sh
 lunch feather_a71-ap3a-user
 mka installclean
